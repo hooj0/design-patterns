b07d341f2d80d5e4ce9ebb7979ea0b69fb96d545
hoojo <hoojo@qq.com>
2018-07-25 23:06:24 +0800

    :sparkles: :bento: Added design patterns `singleton` java example & document

---
+++ src/singleton/java/io/github/hooj0/singleton/support/LazyThreadSafeSingleton.java
@@ -0,0 +1,18 @@
+package io.github.hooj0.singleton.support;
+
+/**
+ * 
+ * @changelog singleton lazy load & thread safe mode
+ * @category singleton
+ * @author hoojo
+ * @createDate 2018年7月25日 下午9:32:14
+ * @file LazyThreadSafeSingleton.java
+ * @package io.github.hooj0.singleton.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class LazyThreadSafeSingleton {
+
+}
