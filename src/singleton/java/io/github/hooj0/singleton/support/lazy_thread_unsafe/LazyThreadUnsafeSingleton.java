ddae6fe7b6ffc8d72e4837f606c44825e84487c4
hoojo <hoojo@qq.com>
2018-08-23 23:18:13 +0800

    :sparkles: :bento: :tada:  Init add 懒汉模式，线程不安全

---
+++ src/singleton/java/io/github/hooj0/singleton/support/lazy_thread_unsafe/LazyThreadUnsafeSingleton.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.singleton.support.lazy_thread_unsafe;
+
+/**
+ * 懒汉模式，线程不安全
+ * @author hoojo
+ * @createDate 2018年8月23日 下午10:57:44
+ * @file LazyThreadUnsafeSingleton.java
+ * @package io.github.hooj0.singleton.support.lazy_thread_unsafe
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class LazyThreadUnsafeSingleton {
+
+}
