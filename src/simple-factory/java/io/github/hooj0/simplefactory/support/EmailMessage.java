a4bc37341db1700318d6ef7648d5b7a6233e2ec6
hoojo <hoojo@qq.com>
2018-09-27 22:58:18 +0800

    :sparkles: :bento: :tada:  Init add 实现 Message 抽象类，提供Email消息实现

---
+++ src/simple-factory/java/io/github/hooj0/simplefactory/support/EmailMessage.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.simplefactory.support;
+
+/**
+ * 实现 Message 抽象类，提供Email消息实现
+ * @author hoojo
+ * @createDate 2018年9月27日 下午10:09:56
+ * @file EmailMessage.java
+ * @package io.github.hooj0.simplefactory.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class EmailMessage extends AbstractMessage {
+
+	@Override
+	public String message() {
+		return "this is a email message.";
+	}
+}
