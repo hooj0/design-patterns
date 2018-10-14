ca8cd5194bff3f0c732b529a320af7189a5532b5
hoojo <hoojo@qq.com>
2018-10-14 10:21:08 +0800

    :sparkles: :bento: :truck:  Renamed change app client package

--- src/factory-method/java/io/github/hooj0/factorymethod/App.java
+++ src/factory-method/java/io/github/hooj0/factorymethod/App.java
@@ -1,4 +1,4 @@
-package io.github.hooj0.factorymethod.support;
+package io.github.hooj0.factorymethod;
 
 import io.github.hooj0.factorymethod.support.factory.AbstractMessageFactory;
 import io.github.hooj0.factorymethod.support.factory.EmailMessageFactory;
@@ -9,7 +9,7 @@ import io.github.hooj0.factorymethod.support.factory.WeChatMessageFactory;
  * @author hoojo
  * @createDate 2018年10月6日 下午6:49:34
  * @file App.java
- * @package io.github.hooj0.factorymethod.support
+ * @package io.github.hooj0.factorymethod
  * @project design-patterns
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
