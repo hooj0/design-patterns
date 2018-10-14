f11bb8276288ee8d428e58629b26038688509793
hoojo <hoojo@qq.com>
2018-10-14 22:42:32 +0800

    :sparkles: :bento: :tada:  Init add simple example, product entity class

---
+++ src/creational/builder/java/io/github/hooj0/builder/support/simple/Product.java
@@ -0,0 +1,86 @@
+package io.github.hooj0.builder.support.simple;
+
+/**
+ * simple example, product entity class
+ * Product的构造使用了Builder，造成了紧耦合，也说明Product需要Builder才能构造对象实例
+ * @author hoojo
+ * @createDate 2018年10月14日 下午10:42:32
+ * @file Product.java
+ * @package io.github.hooj0.builder.support.simple
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public final class Product {
+
+	private final String name;
+	private final float price;
+	private final int size;
+	private final String orderNo;
+
+	private Product(Builder builder) {
+
+		this.name = builder.name;
+		this.price = builder.price;
+		this.size = builder.size;
+		this.orderNo = builder.orderNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public static final class Builder {
+		private final String name;
+		private float price;
+		private int size;
+		private String orderNo;
+
+		public Builder(String name) {
+			if (name == null) {
+				throw new IllegalArgumentException("name can not be null");
+			}
+			this.name = name;
+		}
+		
+		public Builder price(float price) {
+			this.price = price;
+			
+			return this;
+		}
+		
+		public Builder size(int size) {
+			this.size = size;
+			
+			return this;
+		}
+		
+		public Builder orderNo(String orderNo) {
+			this.orderNo = orderNo;
+			
+			return this;
+		}
+		
+		public Product build() {
+			
+			if (this.size <= 0) {
+				throw new IllegalArgumentException("size can not be zore.");
+			}
+			
+			return new Product(this);
+		}
+	}
+}
