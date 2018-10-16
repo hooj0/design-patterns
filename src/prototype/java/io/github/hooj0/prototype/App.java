f48a6bdf765d2664408bc399b1b4854f0864e37d
hoojo <hoojo@qq.com>
2018-10-16 22:55:26 +0800

    :sparkles: :bento: :tada:  Init add prototype pattern app client application

---
+++ src/prototype/java/io/github/hooj0/prototype/App.java
@@ -0,0 +1,28 @@
+package io.github.hooj0.prototype;
+
+import io.github.hooj0.prototype.support.PrototypePatternProduct;
+
+/**
+ * prototype pattern app client application
+ * @author hoojo
+ * @createDate 2018年10月16日 下午10:51:29
+ * @file App.java
+ * @package io.github.hooj0.prototype
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+
+		PrototypePatternProduct product = new PrototypePatternProduct("jack");
+		product.setId(122344L);
+		
+		System.out.println(product);
+		
+		// 克隆对象
+		System.out.println(product.clone());
+	}
+}
