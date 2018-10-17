ecba32964942678e12df17f8986744b56799d847
hoojo <hoojo@qq.com>
2018-10-17 21:09:59 +0800

    :sparkles: :bento: :tada:  Init add object pool application client class

---
+++ src/object-pool/java/io/github/hooj0/objectpool/App.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.objectpool;
+
+/**
+ * object pool application client class
+ * @author hoojo
+ * @createDate 2018年10月17日 下午8:53:07
+ * @file App.java
+ * @package io.github.hooj0.objectpool
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
