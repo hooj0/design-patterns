27b6546ef7ce5b1d2350bd418f8dd04f09361b1f
hoojo <hoojo@qq.com>
2018-10-19 18:13:34 +0800

    :sparkles: :bento: :tada:  Init add bridge pattern application client class

---
+++ src/structural/bridge/java/io/github/hooj0/bridge/App.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.bridge;
+
+/**
+ * bridge pattern application client class
+ * 
+ * @author hoojo
+ * @createDate 2018年10月21日 下午6:12:33
+ * @file App.java
+ * @package io.github.hooj0.bridge
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
