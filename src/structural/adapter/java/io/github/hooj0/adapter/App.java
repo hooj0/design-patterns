6eee6a75aef2fe710bcf2f724d6fd8856eebb96e
hoojo <hoojo@qq.com>
2018-10-19 11:19:34 +0800

    :sparkles: :bento: :tada:  Init add adapter pattern application client class

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/App.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.adapter;
+
+/**
+ * adapter pattern application client class
+ * 
+ * @author hoojo
+ * @createDate 2018年10月18日 上午11:17:17
+ * @file App.java
+ * @package io.github.hooj0.adapter
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
