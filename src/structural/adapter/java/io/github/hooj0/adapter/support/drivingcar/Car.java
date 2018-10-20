087df1ca423083da394e6970823718adfe5b73f7
hoojo <hoojo@qq.com>
2018-10-20 12:58:35 +0800

    :sparkles: :bento: :tada:  Init add car interface defined，implements method driver

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/support/drivingcar/Car.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.adapter.support.drivingcar;
+
+/**
+ * car interface defined，implements method driver
+ * 
+ * 老司机会开小汽车，小汽车的 driver 技能被老司机掌握
+ * @author hoojo
+ * @createDate 2018年10月20日 下午12:12:22
+ * @file Car.java
+ * @package io.github.hooj0.adapter.support.drivingcar
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface Car {
+	
+	void drive();
+}
