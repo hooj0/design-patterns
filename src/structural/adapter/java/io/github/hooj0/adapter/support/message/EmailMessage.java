a680cdf35da0a2c1278a1bf94d9ab6f81db63169
hoojo <hoojo@qq.com>
2018-10-20 12:58:25 +0800

    :sparkles: :bento: :recycle:  Changed email message interface, defined send email method function.

--- src/structural/adapter/java/io/github/hooj0/adapter/support/message/EmailMessage.java
+++ src/structural/adapter/java/io/github/hooj0/adapter/support/message/EmailMessage.java
@@ -2,7 +2,8 @@ package io.github.hooj0.adapter.support.message;
 
 /**
  * email message interface, defined send email method function.
- * 邮件发送接口定义
+ * 邮件发送接口定义，定义发送邮件的功能方法
+ * 
  * @author hoojo
  * @createDate 2018年10月28日 上午10:40:00
  * @file EmailMessage.java
