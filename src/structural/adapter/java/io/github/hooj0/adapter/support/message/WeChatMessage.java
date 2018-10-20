2fc3b8ae5d91157dcbd0555115e4eb419a5b6ce7
hoojo <hoojo@qq.com>
2018-10-20 11:37:23 +0800

    :sparkles: :bento: :tada:  Init add wechat message interface, defined send message method function.

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/support/message/WeChatMessage.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.adapter.support.message;
+
+/**
+ * wechat message interface, defined send message method function.
+ * 发送微信的接口，定义发送微信的姿势
+ * 
+ * @author hoojo
+ * @createDate 2018年10月28日 上午10:45:38
+ * @file WeChatMessage.java
+ * @package io.github.hooj0.adapter.support.message
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface WeChatMessage {
+	
+	void sendMessage();
+}
