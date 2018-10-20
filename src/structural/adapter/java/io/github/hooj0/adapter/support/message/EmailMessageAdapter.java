2f9282366f63cf245e0c16e28835ee5f08b03f5c
hoojo <hoojo@qq.com>
2018-10-20 12:58:26 +0800

    :sparkles: :bento: :recycle:  Changed email message adapter, implements wechat message inteface, help the sender to provide the skills to send wechat.

--- src/structural/adapter/java/io/github/hooj0/adapter/support/message/EmailMessageAdapter.java
+++ src/structural/adapter/java/io/github/hooj0/adapter/support/message/EmailMessageAdapter.java
@@ -2,7 +2,8 @@ package io.github.hooj0.adapter.support.message;
 
 /**
  * email message adapter, implements wechat message inteface, help the sender to provide the skills to send wechat.
- * 由于发送者只会使用邮件，现在需要利用发送邮件的功能伪装成发送微信的功能，去完成发送微信
+ * 由于发送者只会使用微信发送消息，发送者从来没有用过发送邮件这么糟糕的聊天发送，
+ * 现在需要利用发送微信的技能，让发送者利用发送微信的姿势去完成发送邮件进行聊天。
  * 
  * @author hoojo
  * @createDate 2018年10月28日 上午11:03:53
