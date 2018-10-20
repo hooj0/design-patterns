eb439175343eeaf7f3b8febc3c71565f29ca6499
hoojo <hoojo@qq.com>
2018-10-20 12:58:32 +0800

    :sparkles: :bento: :tada:  Init add bus defined interface class，bus support go method

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/support/drivingcar/Bus.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.adapter.support.drivingcar;
+
+/**
+ * bus defined interface class，bus support go method
+ * 巴士可以跑，但老司机还不会开
+ * 
+ * @author hoojo
+ * @createDate 2018年10月20日 下午12:13:54
+ * @file Bus.java
+ * @package io.github.hooj0.adapter.support.drivingcar
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Bus {
+
+	public void go() {
+		System.out.println("bus go go go……");
+	}
+}
