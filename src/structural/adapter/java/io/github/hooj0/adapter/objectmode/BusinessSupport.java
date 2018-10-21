4cc63d1bcfedc51b0dda76f2495be17d66b1b8a9
hoojo <hoojo@qq.com>
2018-10-21 20:30:45 +0800

    :sparkles: :bento: :tada:  Init add business support class, implement method biz.

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/objectmode/BusinessSupport.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.adapter.objectmode;
+
+/**
+ * business support class, implement method biz. 
+ * 系统有一个业务接口，完成了一定的业务。
+ * 
+ * @author hoojo
+ * @createDate 2018年10月21日 下午8:13:01
+ * @file BusinessSupport.java
+ * @package io.github.hooj0.adapter.classmode
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class BusinessSupport {
+
+	public void runBusiness() {
+		System.out.println("run business support.");
+	}
+}
