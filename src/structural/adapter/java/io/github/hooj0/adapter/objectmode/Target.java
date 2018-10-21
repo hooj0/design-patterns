99629e42fa4e5c71ae5d18c0d90036760ae70f6e
hoojo <hoojo@qq.com>
2018-10-21 20:30:47 +0800

    :sparkles: :bento: :tada:  Init add target interface， defined run method & execution method。

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/objectmode/Target.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.adapter.objectmode;
+
+/**
+ * target interface， defined run method & execution method。
+ * 
+ * 有一个目标接口，需要实现两个业务功能。
+ * 
+ * @author hoojo
+ * @createDate 2018年10月21日 下午8:15:00
+ * @file Target.java
+ * @package io.github.hooj0.adapter.classmode
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface Target {
+
+	void run();
+	
+	void execute();
+}
