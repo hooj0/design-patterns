6fc4720c072095d5199c1071144a3f93d3cc6e30
hoojo <hoojo@qq.com>
2018-10-22 20:46:22 +0800

    :sparkles: :bento: :tada:  Init add current system existing business interface.

---
+++ src/structural/adapter/java/io/github/hooj0/adapter/interfacemode/Business.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.adapter.interfacemode;
+
+/**
+ * current system existing business interface.
+ * 当前系统中已有的业务接口
+ * 
+ * @author hoojo
+ * @createDate 2018年10月22日 下午8:33:28
+ * @file Business.java
+ * @package io.github.hooj0.adapter.interfacemode
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface Business {
+
+	public void runBusiness();
+	
+	public void execBusiness();
+}
