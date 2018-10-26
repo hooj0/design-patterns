23758aa58811ff80f5aeb1ca7092f0a0f6cd5036
hoojo <hoojo@qq.com>
2018-10-26 23:17:52 +0800

    :sparkles: :bento: :tada:  Init add composite pattern —— tree node class defined

---
+++ src/structural/composite/java/io/github/hooj0/composite/support/TreeNode.java
@@ -0,0 +1,18 @@
+package io.github.hooj0.composite.support;
+
+/**
+ * composite pattern —— tree node class defined 
+ * 树形节点元素对象定义
+ * 
+ * @author hoojo
+ * @createDate 2018年10月26日 下午11:14:46
+ * @file TreeNode.java
+ * @package io.github.hooj0.composite.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class TreeNode {
+
+}
