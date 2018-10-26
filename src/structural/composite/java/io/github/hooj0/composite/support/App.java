73647730e062efbdbd3bf89e9efb70cfdfa79857
hoojo <hoojo@qq.com>
2018-10-26 23:00:42 +0800

    :sparkles: :bento: :tada:  Init add composite pattern application clien class

---
+++ src/structural/composite/java/io/github/hooj0/composite/support/App.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.composite.support;
+
+/**
+ * composite pattern application clien class
+ * 
+ * @author hoojo
+ * @createDate 2018年10月26日 下午10:58:53
+ * @file App.java
+ * @package io.github.hooj0.composite.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
