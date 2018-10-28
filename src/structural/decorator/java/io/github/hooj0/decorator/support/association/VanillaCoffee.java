eb81135ffcf12ca4c5b2b47975e63c7bc92fd23b
hoojo <hoojo@qq.com>
2018-10-28 23:28:41 +0800

    :sparkles: :bento: :tada:  Init add vanilla coffee  interface support

---
+++ src/structural/decorator/java/io/github/hooj0/decorator/support/association/VanillaCoffee.java
@@ -0,0 +1,34 @@
+package io.github.hooj0.decorator.support.association;
+
+/**
+ * vanilla coffee  interface support
+ * 香草咖啡业务对象实现类
+ * 
+ * @author hoojo
+ * @createDate 2018年10月28日 下午11:17:50
+ * @file VanillaCoffee.java
+ * @package io.github.hooj0.decorator.support.association
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class VanillaCoffee implements Coffee {
+
+	private Coffee decorated;
+
+	public VanillaCoffee(Coffee coffee) {
+		this.decorated = coffee;
+	}
+	
+	@Override
+	public int getCost() {
+		return decorated.getCost() + 5;
+	}
+
+	@Override
+	public String getDescription() {
+		return decorated.getDescription() + ", add vanilla";
+	}
+
+}
