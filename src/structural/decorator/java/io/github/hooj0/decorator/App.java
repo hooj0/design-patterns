caac58939dda16d23778291bcc27f00d01e95f49
hoojo <hoojo@qq.com>
2018-10-28 23:28:33 +0800

    :sparkles: :bento: :recycle:  Changed deacorator pattern app client class

--- src/structural/decorator/java/io/github/hooj0/decorator/App.java
+++ src/structural/decorator/java/io/github/hooj0/decorator/App.java
@@ -1,5 +1,11 @@
 package io.github.hooj0.decorator;
 
+import io.github.hooj0.decorator.support.association.Coffee;
+import io.github.hooj0.decorator.support.association.HoneyCoffee;
+import io.github.hooj0.decorator.support.association.MilkCoffee;
+import io.github.hooj0.decorator.support.association.SimpleCoffee;
+import io.github.hooj0.decorator.support.association.VanillaCoffee;
+
 /**
  * deacorator pattern app client class
  * 
@@ -16,5 +22,22 @@ public class App {
 
 	public static void main(String[] args) {
 
+		Coffee coffee = new SimpleCoffee();
+		
+		MilkCoffee milkCoffee = new MilkCoffee(coffee);
+		System.out.println(milkCoffee.getCost()); // 10
+		System.out.println(milkCoffee.getDescription()); // ordinary coffe, add milk
+		
+		VanillaCoffee vanillaCoffee = new VanillaCoffee(milkCoffee);
+		System.out.println(vanillaCoffee.getCost()); // 15
+		System.out.println(vanillaCoffee.getDescription()); // ordinary coffe, add milk, add vanilla
+		
+		HoneyCoffee honeyCoffee = new HoneyCoffee(vanillaCoffee);
+		System.out.println(honeyCoffee.getCost()); // 18
+		System.out.println(honeyCoffee.getDescription()); // ordinary coffe, add milk, add vanilla, add honey
+
+		VanillaCoffee vanillaCoffee2 = new VanillaCoffee(coffee);
+		System.out.println(vanillaCoffee2.getCost()); // 13
+		System.out.println(vanillaCoffee2.getDescription()); // ordinary coffe, add vanilla
 	}
 }
