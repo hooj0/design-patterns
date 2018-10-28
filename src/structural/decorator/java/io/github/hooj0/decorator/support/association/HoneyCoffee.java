338b385fd63dc90158c36b0eba9339c4af4495d8
hoojo <hoojo@qq.com>
2018-10-28 23:28:36 +0800

    :sparkles: :bento: :tada:  Init add honey coffee  interface support

---
+++ src/structural/decorator/java/io/github/hooj0/decorator/support/association/HoneyCoffee.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.decorator.support.association;
+
+/**
+ * honey coffee  interface support
+ * 蜂蜜咖啡业务对象实现类
+ * 
+ * @author hoojo
+ * @createDate 2018年10月28日 下午11:22:27
+ * @file HoneyCoffee.java
+ * @package io.github.hooj0.decorator.support.association
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class HoneyCoffee implements Coffee {
+	
+	private Coffee decorated;
+
+	public HoneyCoffee(Coffee coffee) {
+		this.decorated = coffee;
+	}
+	
+	@Override
+	public int getCost() {
+		return decorated.getCost() + 3;
+	}
+
+	@Override
+	public String getDescription() {
+		return decorated.getDescription() + ", add honey";
+	}
+}
