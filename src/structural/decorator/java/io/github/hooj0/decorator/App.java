2d98f78b5bd75d1f91ba7e02b2d79876e45608c6
hoojo <hoojo@qq.com>
2018-10-28 14:58:36 +0800

    :sparkles: :bento: :tada:  Init add deacorator pattern app client class

---
+++ src/structural/decorator/java/io/github/hooj0/decorator/App.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.decorator;
+
+/**
+ * deacorator pattern app client class
+ * 
+ * @author hoojo
+ * @createDate 2018年10月28日 下午2:57:19
+ * @file App.java
+ * @package io.github.hooj0.decorator
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
