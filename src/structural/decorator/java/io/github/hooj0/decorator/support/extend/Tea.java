15113642cf6474f9a7a88c0616514d9bffc864f1
hoojo <hoojo@qq.com>
2018-10-29 12:20:19 +0800

    :sparkles: :bento: :tada:  Init add tea interface defined method

---
+++ src/structural/decorator/java/io/github/hooj0/decorator/support/extend/Tea.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.decorator.support.extend;
+
+/**
+ * tea interface defined method
+ * 茶接口定义，提供价格和描述相关业务方法
+ * 
+ * @author hoojo
+ * @createDate 2018年10月29日 上午8:06:25
+ * @file Tea.java
+ * @package io.github.hooj0.decorator.support.extend
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface Tea {
+	
+	int getCost();
+	
+	String getDescription();
+}
