5cd7ca2bc606baee392cd765f321971475f68f49
hoojo <hoojo@qq.com>
2018-10-30 16:19:01 +0800

    :sparkles: :bento: :tada:  Init add facade pattern — application client class

---
+++ src/structural/facade/java/io/github/hooj0/facade/App.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.facade;
+
+/**
+ * facade pattern — application client class
+ * 
+ * @author hoojo
+ * @createDate 2018年10月30日 下午4:14:29
+ * @file App.java
+ * @package io.github.hooj0.facade
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
