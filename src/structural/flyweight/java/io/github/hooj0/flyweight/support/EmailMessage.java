0765f3cd6aa654d1ae3b9e0473a3485aab3029ee
hoojo <hoojo@qq.com>
2018-11-03 23:09:51 +0800

    :sparkles: :bento: :tada:  Init add email message sender support

---
+++ src/structural/flyweight/java/io/github/hooj0/flyweight/support/EmailMessage.java
@@ -0,0 +1,25 @@
+package io.github.hooj0.flyweight.support;
+
+/**
+ * email message sender support
+ * 
+ * @author hoojo
+ * @createDate 2018年11月7日 下午10:49:39
+ * @file EmailMessage.java
+ * @package io.github.hooj0.flyweight.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class EmailMessage extends AbstractMessage {
+
+	public EmailMessage(String type) {
+		super(type);
+	}
+
+	@Override
+	String messageFooter() {
+		return "email message footer! @" + System.identityHashCode(this);
+	}
+}
