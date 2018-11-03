17dff05de3a83b78978abb5b0d1b43e9e0416e16
hoojo <hoojo@qq.com>
2018-11-03 23:09:50 +0800

    :sparkles: :bento: :tada:  Init add abstract message interface support，defined abstract method.

---
+++ src/structural/flyweight/java/io/github/hooj0/flyweight/support/AbstractMessage.java
@@ -0,0 +1,34 @@
+package io.github.hooj0.flyweight.support;
+
+/**
+ * abstract message interface support，defined abstract method.
+ * 消息发送接口抽象实现，完成接口方法的组合，定义子类需要实现的方法
+ * 
+ * @author hoojo
+ * @createDate 2018年11月7日 下午10:37:21
+ * @file AbstractMessage.java
+ * @package io.github.hooj0.flyweight.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class AbstractMessage implements Message {
+
+	private String type;
+	
+	public AbstractMessage(String type) {
+		this.type = type;
+	}
+	
+	private String messageHeader() {
+		return this.type + " message header "; 
+	}
+	
+	public void sendMessage(String text) {
+		
+		System.out.println(messageHeader() + ", content: " + text + ", " + messageFooter() + "#" + System.identityHashCode(this));
+	}
+	
+	abstract String messageFooter();
+}
