cf51e12a022acd0cc34096694b8b597370d95e4b
hoojo <hoojo@qq.com>
2018-11-14 23:12:11 +0800

    :sparkles: :bento: :tada:  Init add Subject Interface: loader image interface class

---
+++ src/structural/proxy/java/io/github/hooj0/proxy/_static/support/ImageLoader.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.proxy._static.support;
+
+/**
+ * Subject Interface: loader image interface class
+ * 主题业务接口定义需要实现的业务方法，代理对象和真实主题对象都需要继承或实现该角色接口
+ * 
+ * @author hoojo
+ * @createDate 2018年11月14日 下午10:58:57
+ * @file ImageLoader.java
+ * @package io.github.hooj0.proxy._static.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface ImageLoader {
+
+	public void loader();
+}
