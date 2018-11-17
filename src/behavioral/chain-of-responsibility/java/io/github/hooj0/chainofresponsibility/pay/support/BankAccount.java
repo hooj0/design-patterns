a076d9a3ab51c1e34a59d0eb2d9f9a3a0e38f599
hoojo <hoojo@qq.com>
2018-11-17 16:17:56 +0800

    :sparkles: :bento: :tada:  Init add bank pay account chain class, extend account

---
+++ src/behavioral/chain-of-responsibility/java/io/github/hooj0/chainofresponsibility/pay/support/BankAccount.java
@@ -0,0 +1,26 @@
+package io.github.hooj0.chainofresponsibility.pay.support;
+
+/**
+ * bank pay account chain class, extend account 
+ * 银行账户支付链
+ * 
+ * @author hoojo
+ * @createDate 2018年11月17日 下午3:35:18
+ * @file BankAccount.java
+ * @package io.github.hooj0.chainofresponsibility.pay.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class BankAccount extends Account {
+
+	public BankAccount(Account next, int balance) {
+		super(next, balance);
+	}
+
+	@Override
+	public String accountType() {
+		return "ICBC BANK";
+	}
+}
