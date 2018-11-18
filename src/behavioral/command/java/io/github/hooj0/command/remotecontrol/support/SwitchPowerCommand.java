85a1125051110916f84d5cb28d35aab49a63ba8b
hoojo <hoojo@qq.com>
2018-11-18 20:49:24 +0800

    :sparkles: :bento: :tada:  Init add remote control switch power command interface

---
+++ src/behavioral/command/java/io/github/hooj0/command/remotecontrol/support/SwitchPowerCommand.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.command.remotecontrol.support;
+
+/**
+ * remote control switch power command interface
+ * 开关遥控器命令接口
+ * 
+ * @author hoojo
+ * @createDate 2018年11月18日 下午8:05:23
+ * @file SwitchPowerCommand.java
+ * @package io.github.hooj0.command.remotecontrol.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface SwitchPowerCommand {
+
+	void execute();
+}
