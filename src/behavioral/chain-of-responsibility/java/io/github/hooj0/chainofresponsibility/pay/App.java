b16a2b86554170a2b30a3451b57e65f70173ebd5
hoojo <hoojo@qq.com>
2018-11-18 10:17:37 +0800

    :sparkles: :bento: :recycle:  Changed chain of responsibility application client

--- src/behavioral/chain-of-responsibility/java/io/github/hooj0/chainofresponsibility/pay/App.java
+++ src/behavioral/chain-of-responsibility/java/io/github/hooj0/chainofresponsibility/pay/App.java
@@ -1,9 +1,6 @@
 package io.github.hooj0.chainofresponsibility.pay;
 
-import io.github.hooj0.chainofresponsibility.pay.support.Account;
-import io.github.hooj0.chainofresponsibility.pay.support.AlipayAccount;
-import io.github.hooj0.chainofresponsibility.pay.support.BankAccount;
-import io.github.hooj0.chainofresponsibility.pay.support.BitcoinAccount;
+import io.github.hooj0.chainofresponsibility.pay.support.PayChain;
 
 /**
  * chain of responsibility application client
@@ -21,14 +18,11 @@ public class App {
 
 	public static void main(String[] args) {
 
-		// 构建一个支付账户的链条
-		Account account = new BankAccount(null, 300);
-		account = new BitcoinAccount(account, 400);
-		account = new AlipayAccount(account, 200);
+		PayChain chain = new PayChain();
 		
-		account.play(350); // Account [Bitcoin] pay amount '350' successful, balance is '50'.
-		account.play(130); // Account [alipay] pay amount '130' successful, balance is '70'.
-		account.play(240); // Account [ICBC BANK] pay amount '240' successful, balance is '60'.
+		chain.pay(350); // Account [Bitcoin] pay amount '350' successful, balance is '50'.
+		chain.pay(130); // Account [alipay] pay amount '130' successful, balance is '70'.
+		chain.pay(240); // Account [ICBC BANK] pay amount '240' successful, balance is '60'.
 
 		/*
 		 	Account [Bitcoin] pay amount '350' successful, balance is '50'.
