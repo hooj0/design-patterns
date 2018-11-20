e665e305aa7de00561d75605b0daf3417902645d
hoojo <hoojo@qq.com>
2018-11-20 21:57:39 +0800

    :sparkles: :bento: :truck:  Renamed red font command implements class

--- src/behavioral/command/java/io/github/hooj0/command/typewriter/support/FontColorCommand.java
+++ src/behavioral/command/java/io/github/hooj0/command/typewriter/support/FontColorCommand.java
@@ -5,40 +5,36 @@ package io.github.hooj0.command.typewriter.support;
  * 
  * @author hoojo
  * @createDate 2018年11月19日 下午10:46:37
- * @file RedFont.java
+ * @file FontColorCommand.java
  * @package io.github.hooj0.command.typewriter.support
  * @project design-patterns
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
  * @version 1.0
  */
-public class RedFontCommand extends Command {
+public class FontColorCommand extends Command {
 
 	private AbstractFont font;
-	private Color originalColor;
-	
-	public RedFontCommand(AbstractFont font) {
-		this.font = font;
-		this.originalColor = this.font.getColor();
-	}
 	
 	@Override
-	public void execute() {
+	public void execute(AbstractFont font) {
+		this.font = font;
+		
 		this.font.setColor(Color.RED);
 	}
 
 	@Override
 	public void undo() {
-		this.font.setColor(originalColor);
+		this.font.setColor(Color.NORMAL);
 	}
 
 	@Override
 	public void redo() {
-		this.execute();
+		this.execute(this.font);
 	}
 
 	@Override
 	public String toString() {
-		return "设置红色字体";
+		return "font-color";
 	}
 }
