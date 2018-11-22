7778959ba0f47f18a5fd1de3cd6d7c1de7b2e543
hoojo <hoojo@qq.com>
2018-11-22 22:36:23 +0800

    :sparkles: :bento: :tada:  Init add divide expression support

---
+++ src/behavioral/interpreter/java/io/github/hooj0/interpreter/support/DivideExpression.java
@@ -0,0 +1,28 @@
+package io.github.hooj0.interpreter.support;
+
+/**
+ * divide expression support
+ * 除法表达式实现
+ * @author hoojo
+ * @createDate 2018年11月22日 下午10:33:23
+ * @file DivideExpression.java
+ * @package io.github.hooj0.interpreter.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class DivideExpression extends Expression {
+
+	@Override
+	public void interpret() {
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
