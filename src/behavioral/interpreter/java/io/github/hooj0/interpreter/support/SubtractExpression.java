87cfde5a1f848e37c00d23de102b0c6688871b2c
hoojo <hoojo@qq.com>
2018-11-22 22:36:27 +0800

    :sparkles: :bento: :tada:  Init add subtract expression support

---
+++ src/behavioral/interpreter/java/io/github/hooj0/interpreter/support/SubtractExpression.java
@@ -0,0 +1,29 @@
+package io.github.hooj0.interpreter.support;
+
+/**
+ * subtract expression support
+ * 减法表达式实现
+ * @author hoojo
+ * @createDate 2018年11月22日 下午10:31:16
+ * @file SubtractExpression.java
+ * @package io.github.hooj0.interpreter.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class SubtractExpression extends Expression {
+
+	@Override
+	public void interpret() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
