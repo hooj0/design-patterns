4ef177c875f428efabb736f5dd95536062837b40
hoojo <hoojo@qq.com>
2018-11-22 22:36:22 +0800

    :sparkles: :bento:  Added  expression support

---
+++ src/behavioral/interpreter/java/io/github/hooj0/interpreter/support/AddExpression.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.interpreter.support;
+
+/**
+ * add expression support
+ * 加法表达式实现
+ * 
+ * @author hoojo
+ * @createDate 2018年11月22日 下午10:30:20
+ * @file AddExpression.java
+ * @package io.github.hooj0.interpreter.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class AddExpression extends Expression {
+
+	@Override
+	public void interpret() {
+		
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+}
