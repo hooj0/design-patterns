4dedd98d06ffae65edb567aab202b1c78fef46ee
hoojo <hoojo@qq.com>
2018-11-26 23:27:52 +0800

    :sparkles: :bento: :tada:  Init add chat room mediator

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/support/ChatRoomMediator.java
@@ -0,0 +1,18 @@
+package io.github.hooj0.mediator.support;
+
+/**
+ * chat room mediator 
+ * 聊天室中介者接口
+ * @author hoojo
+ * @createDate 2018年11月26日 下午10:35:38
+ * @file ChatRoomMediator.java
+ * @package io.github.hooj0.mediator.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface ChatRoomMediator {
+
+	public void showMessage(User user, String message);
+}
