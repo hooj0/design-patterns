0da8826158c32eb513fc065a264fd05805f03455
hoojo <hoojo@qq.com>
2018-11-29 23:22:40 +0800

    :sparkles: :bento: :tada:  Init add party member mediator abstract class

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/AbstractPartyMember.java
@@ -0,0 +1,17 @@
+package io.github.hooj0.mediator.party.support;
+
+/**
+ * party member mediator abstract class
+ * 派对成员抽象实现
+ * @author hoojo
+ * @createDate 2018年11月29日 下午10:46:58
+ * @file AbstractPartyMember.java
+ * @package io.github.hooj0.mediator.party.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class AbstractPartyMember {
+
+}
