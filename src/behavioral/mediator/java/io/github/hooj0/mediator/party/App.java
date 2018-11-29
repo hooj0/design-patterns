065b470ec4c30494f7721e08ffbbdb4278c64e65
hoojo <hoojo@qq.com>
2018-11-29 23:22:38 +0800

    :sparkles: :bento: :tada:  Init add party mediator application client

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/App.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.mediator.party;
+
+/**
+ * party mediator application client
+ * @author hoojo
+ * @createDate 2018年11月29日 下午10:47:20
+ * @file App.java
+ * @package io.github.hooj0.mediator.party
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
