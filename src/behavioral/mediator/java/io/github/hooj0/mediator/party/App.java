614ea33caef2d74922103c4e0a5ea670ec3387cb
hoojo <hoojo@qq.com>
2018-12-01 10:08:59 +0800

    :sparkles: :bento: :recycle:  Changed party mediator application client

--- src/behavioral/mediator/java/io/github/hooj0/mediator/party/App.java
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/App.java
@@ -1,5 +1,14 @@
 package io.github.hooj0.mediator.party;
 
+import io.github.hooj0.mediator.party.support.Boss;
+import io.github.hooj0.mediator.party.support.Client;
+import io.github.hooj0.mediator.party.support.Party;
+import io.github.hooj0.mediator.party.support.PartyImpl;
+import io.github.hooj0.mediator.party.support.PartyMember;
+import io.github.hooj0.mediator.party.support.Shareholder;
+import io.github.hooj0.mediator.party.support.ShowType;
+import io.github.hooj0.mediator.party.support.Staff;
+
 /**
  * party mediator application client
  * @author hoojo
@@ -14,6 +23,23 @@ package io.github.hooj0.mediator.party;
 public class App {
 
 	public static void main(String[] args) {
-
+		Party party = new PartyImpl();
+		
+		PartyMember staff = new Staff();
+		PartyMember boss = new Boss();
+		PartyMember shareholder = new Shareholder();
+		PartyMember client = new Client();
+		
+		// add member
+		party.addMember(staff);
+		party.addMember(boss);
+		party.addMember(shareholder);
+		party.addMember(client);
+		
+		// party show start
+		staff.revelShow(ShowType.TRUTH_OR_DARE);
+		boss.revelShow(ShowType.DESKTOP_GAME);
+		shareholder.revelShow(ShowType.DANCE);
+		client.revelShow(ShowType.FOOD);
 	}
 }
