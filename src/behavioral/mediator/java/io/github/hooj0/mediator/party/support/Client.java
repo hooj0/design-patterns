5e3450b84ab60d284ed161d6b31a74a14fbed3e6
hoojo <hoojo@qq.com>
2018-12-01 10:09:07 +0800

    :sparkles: :bento: :tada:  Init add company client memeber

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/Client.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.mediator.party.support;
+
+/**
+ * company client memeber
+ * @author hoojo
+ * @createDate 2018年12月1日 上午10:00:28
+ * @file Client.java
+ * @package io.github.hooj0.mediator.party.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Client extends AbstractPartyMember {
+
+	@Override
+	public String toString() {
+		return "client";
+	}
+}
