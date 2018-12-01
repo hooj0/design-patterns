890ab68545485fefc7df422a2dfd82c75bbdd8c5
hoojo <hoojo@qq.com>
2018-12-01 10:09:12 +0800

    :sparkles: :bento: :tada:  Init add company staff member

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/Staff.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.mediator.party.support;
+
+/**
+ * company staff member
+ * @author hoojo
+ * @createDate 2018年12月1日 上午9:57:49
+ * @file Staff.java
+ * @package io.github.hooj0.mediator.party.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Staff extends AbstractPartyMember {
+
+	@Override
+	public String toString() {
+		return "staff";
+	}
+}
