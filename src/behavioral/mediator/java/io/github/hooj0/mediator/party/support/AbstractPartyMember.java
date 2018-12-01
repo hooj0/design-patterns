3e3d7a972188a6941c6cca18cb12ef0ffd08286d
hoojo <hoojo@qq.com>
2018-12-01 10:09:01 +0800

    :sparkles: :bento: :recycle:  Changed party member mediator interface class, members need to join the party and participate in party shows and party carnivals.

--- src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/AbstractPartyMember.java
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/AbstractPartyMember.java
@@ -1,8 +1,9 @@
 package io.github.hooj0.mediator.party.support;
 
 /**
- * party member mediator abstract class
- * 派对成员抽象实现
+ * party member mediator interface class, members need to join the party and participate in party shows and party carnivals.
+ * 派对成员接口定义，成员需要加入派对，并且参与派对秀和派对狂欢
+ * 
  * @author hoojo
  * @createDate 2018年11月29日 下午10:46:58
  * @file AbstractPartyMember.java
@@ -12,6 +13,29 @@ package io.github.hooj0.mediator.party.support;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public abstract class AbstractPartyMember {
+public abstract class AbstractPartyMember implements PartyMember {
 
+	private Party party;
+	
+	@Override
+	public void joinParty(Party party) {
+		this.party = party;
+		
+		System.out.println(this + " join the party.");
+	}
+
+	@Override
+	public void partyShow(ShowType type) {
+		System.out.println(this + " enjoy " + type.getDescription());
+	}
+
+	@Override
+	public void revelShow(ShowType type) {
+		if (party != null) {
+			System.out.println(this + " " + type + " " + type.getDescription());
+		}
+	}
+	
+	@Override
+	public abstract String toString();
 }
