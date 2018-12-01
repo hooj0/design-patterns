39d0cdda70b0c5b9a813811cc1fb06f4e4ff56c2
hoojo <hoojo@qq.com>
2018-12-01 10:09:06 +0800

    :sparkles: :bento: :tada:  Init add boss party memeber

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/Boss.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.mediator.party.support;
+
+/**
+ * boss party memeber
+ * 
+ * @author hoojo
+ * @createDate 2018年12月1日 上午9:55:35
+ * @file Boss.java
+ * @package io.github.hooj0.mediator.party.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Boss extends AbstractPartyMember {
+
+	@Override
+	public String toString() {
+		return "boss";
+	}
+}
