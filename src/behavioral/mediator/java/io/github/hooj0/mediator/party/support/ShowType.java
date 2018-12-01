fb2762535f165ab80cb50f127ac99a9cf304ab43
hoojo <hoojo@qq.com>
2018-12-01 10:09:05 +0800

    :sparkles: :bento: :recycle:  Changed party show type enum class

--- src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/ShowType.java
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/ShowType.java
@@ -1,7 +1,8 @@
 package io.github.hooj0.mediator.party.support;
 
 /**
- * party show type enum class 派对的节目表演秀种类
+ * party show type enum class 
+ * 派对的节目表演秀种类
  * 
  * @author hoojo
  * @createDate 2018年11月29日 下午10:58:51
@@ -13,10 +14,12 @@ package io.github.hooj0.mediator.party.support;
  * @version 1.0
  */
 public enum ShowType {
-	SHOOT("射击", "Shooting"), 
-	GUESS("猜灯谜", "Guess"), 
+	DARTS("飞镖", "Darts"), 
+	TRUTH_OR_DARE("真心话大冒险", "truth or Dare"), 
 	DESKTOP_GAME("桌游", "Desktop games"), 
-	SING("唱歌", "singing");
+	SING("唱歌", "singing"),
+	DANCE("跳舞", "dance"),
+	FOOD("美食", "food");
 
 	private String name;
 	private String description;
