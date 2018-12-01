cc353828e07f5cd5d687da253e9acf229955f909
hoojo <hoojo@qq.com>
2018-12-01 10:09:10 +0800

    :sparkles: :bento: :tada:  Init add company shareholder member

---
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/Shareholder.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.mediator.party.support;
+
+/**
+ * company shareholder member 
+ * 
+ * @author hoojo
+ * @createDate 2018年12月1日 上午9:59:04
+ * @file Shareholder.java
+ * @package io.github.hooj0.mediator.party.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Shareholder extends AbstractPartyMember {
+
+	@Override
+	public String toString() {
+		return "shareholder";
+	}
+}
