ca6e7cb4aad5d07ab93753ee7610b834be43dc32
hoojo <hoojo@qq.com>
2018-12-01 10:09:04 +0800

    :sparkles: :bento: :recycle:  Changed party member mediator interface class, members need to join the party and participate in party shows and party carnivals.

--- src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/PartyMember.java
+++ src/behavioral/mediator/java/io/github/hooj0/mediator/party/support/PartyMember.java
@@ -1,8 +1,8 @@
 package io.github.hooj0.mediator.party.support;
 
 /**
- * party member mediator interface class
- * 派对成员接口定义
+ * party member mediator interface class, members need to join the party and participate in party shows and party carnivals.
+ * 派对成员接口定义，成员需要加入派对，并且参与派对秀和派对狂欢
  * @author hoojo
  * @createDate 2018年11月29日 下午10:52:10
  * @file PartyMember.java
@@ -19,5 +19,5 @@ public interface PartyMember {
 	public void partyShow(ShowType type);
 	
 	// 狂欢
-	public void revel(ShowType type);
+	public void revelShow(ShowType type);
 }
