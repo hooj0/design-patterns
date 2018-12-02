86c6064ab907f3076128dcf576ffdc64726a97da
hoojo <hoojo@qq.com>
2018-12-02 23:28:26 +0800

    :sparkles: :bento: :tada:  Init add text editor implements editor

---
+++ src/behavioral/memento/java/io/github/hooj0/memento/editor/support/TextEditor.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.memento.editor.support;
+
+/**
+ * text editor implements editor
+ * 
+ * 文本编辑器实现
+ * @author hoojo
+ * @createDate 2018年12月6日 下午11:23:00
+ * @file TextEditor.java
+ * @package io.github.hooj0.memento.editor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class TextEditor implements Editor {
+
+	@Override
+	public String getContent() {
+		return null;
+	}
+
+	@Override
+	public Memento save() {
+		return null;
+	}
+
+	@Override
+	public void restore(Memento memento) {
+		
+	}
+
+}
