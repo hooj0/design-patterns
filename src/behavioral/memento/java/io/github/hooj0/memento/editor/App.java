4f22bbf215e896b98f13bed58967ff4badbb2d8a
hoojo <hoojo@qq.com>
2018-12-02 23:28:20 +0800

    :sparkles: :bento: :tada:  Init add memento pattern editor application client

---
+++ src/behavioral/memento/java/io/github/hooj0/memento/editor/App.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.memento.editor;
+
+/**
+ * memento pattern editor application client
+ * @author hoojo
+ * @createDate 2018年12月6日 下午11:12:25
+ * @file App.java
+ * @package io.github.hooj0.memento.editor
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
