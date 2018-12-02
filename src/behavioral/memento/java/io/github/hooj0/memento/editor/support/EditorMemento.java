029449715158d39c9ee42ad048073cd8593fc149
hoojo <hoojo@qq.com>
2018-12-02 23:28:23 +0800

    :sparkles: :bento: :tada:  Init add memento interface class implements

---
+++ src/behavioral/memento/java/io/github/hooj0/memento/editor/support/EditorMemento.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.memento.editor.support;
+
+/**
+ * memento interface class implements
+ * 备忘录接口实现
+ * 
+ * @author hoojo
+ * @createDate 2018年12月6日 下午11:21:01
+ * @file EditorMemento.java
+ * @package io.github.hooj0.memento.editor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class EditorMemento implements Memento {
+
+	@Override
+	public String getContent() {
+		return null;
+	}
+}
