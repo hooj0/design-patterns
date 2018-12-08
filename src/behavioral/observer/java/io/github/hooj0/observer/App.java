930ff048064a3810e51ede8acbcd39281062cb2a
hoojo <hoojo@qq.com>
2018-12-08 09:51:23 +0800

    :sparkles: :bento: :tada:  Init add observer pattern application client

---
+++ src/behavioral/observer/java/io/github/hooj0/observer/App.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.observer;
+
+/**
+ * observer pattern application client
+ * @author hoojo
+ * @createDate 2018年12月8日 上午9:49:33
+ * @file App.java
+ * @package io.github.hooj0.observer
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+}
