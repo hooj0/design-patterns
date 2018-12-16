63b19e3fdca76f5a4add61189842b729bc4e5ac9
hoojo <hoojo@qq.com>
2018-12-16 21:48:47 +0800

    :sparkles: :bento: :tada:  Init add keyboard press and freed state

---
+++ src/behavioral/state/java/io/github/hooj0/state/support/Keyboard.java
@@ -0,0 +1,40 @@
+package io.github.hooj0.state.support;
+
+/**
+ * keyboard press and freed state
+ * 键盘按下和释放状态
+ * 
+ * @author hoojo
+ * @createDate 2018年12月16日 下午9:13:35
+ * @file Keyboard.java
+ * @package io.github.hooj0.state.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Keyboard {
+
+	private State state;
+	
+	public Keyboard() {
+		this.state = new KeyPressState(this);
+	}
+	
+	private void changeStateTo(State newState) {
+		this.state = newState;
+		this.state.onEnterState();
+	}
+	
+	public void rap() {
+		this.state.onSpeedState();
+	}
+	
+	public void recovery() {
+		if (this.state.getClass().equals(KeyPressState.class)) {
+			this.changeStateTo(new KeyFreedState(this));
+		} else {
+			this.changeStateTo(new KeyPressState(this));
+		}
+	}
+}
