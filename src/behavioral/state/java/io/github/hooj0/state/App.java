eeb7353147f066d05a9fc8d95bc04e8647eff02d
hoojo <hoojo@qq.com>
2018-12-16 21:48:43 +0800

    :sparkles: :bento: :tada:  Init add state pattern application client

---
+++ src/behavioral/state/java/io/github/hooj0/state/App.java
@@ -0,0 +1,27 @@
+package io.github.hooj0.state;
+
+import io.github.hooj0.state.support.Keyboard;
+
+/**
+ * state pattern application client
+ * @author hoojo
+ * @createDate 2018年12月16日 下午8:58:08
+ * @file App.java
+ * @package io.github.hooj0.state
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+		Keyboard keyboard = new Keyboard();
+		
+		keyboard.recovery();
+		keyboard.rap();
+		
+		keyboard.recovery();
+		keyboard.rap();
+	}
+}
