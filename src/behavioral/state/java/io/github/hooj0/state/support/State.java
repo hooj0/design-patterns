11149656edfecd4070128e534ea474525c228dc0
hoojo <hoojo@qq.com>
2018-12-16 21:48:48 +0800

    :sparkles: :bento: :tada:  Init add state interface class defined.

---
+++ src/behavioral/state/java/io/github/hooj0/state/support/State.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.state.support;
+
+/**
+ * state interface class defined.
+ * 状态接口定义
+ * 
+ * @author hoojo
+ * @createDate 2018年12月16日 下午8:59:19
+ * @file State.java
+ * @package io.github.hooj0.state.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface State {
+
+	public void onEnterState();
+	
+	public void onSpeedState();
+}
