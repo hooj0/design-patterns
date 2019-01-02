1b5f29c1fc18238ce723d38f81a2d597415305cf
hoojo <hoojo@qq.com>
2019-01-02 22:57:15 +0800

    :sparkles: :bento: :tada:  Init add visitor pattern element abstract support

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/AbstractComputerComponent.java
@@ -0,0 +1,17 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * visitor pattern element abstract support
+ * Element 角色抽象实现
+ * @author hoojo
+ * @createDate 2019年1月2日 下午10:35:58
+ * @file AbstractComputerComponent.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class AbstractComputerComponent implements ComputerComponent {
+
+}
