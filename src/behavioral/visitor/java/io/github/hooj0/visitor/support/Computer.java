6bfd1835e791cbc28be67193203d42bb97243904
hoojo <hoojo@qq.com>
2019-01-02 21:52:27 +0800

    :sparkles: :bento: :tada:  Init add computer component visitor element

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Computer.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * computer component visitor element
+ * 电脑  Element 抽象实现
+ * @author hoojo
+ * @createDate 2019年1月3日 下午9:42:33
+ * @file Computer.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Computer extends AbstractElectronicDevice {
+
+	@Override
+	public void accept(ElectronicDeviceVisitor deviceVisitor) {
+		
+	}
+}
