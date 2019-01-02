f2224a2ecb2ffc230e113fc51a494dc7a2055280
hoojo <hoojo@qq.com>
2019-01-02 21:52:23 +0800

    :sparkles: :bento: :recycle:  Changed keyboard visitor element support

--- src/behavioral/visitor/java/io/github/hooj0/visitor/support/Keyboard.java
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Keyboard.java
@@ -1,8 +1,8 @@
 package io.github.hooj0.visitor.support;
 
 /**
- * keyboard visitor element
- * 
+ * keyboard visitor element support
+ * 键盘 Element 角色抽象实现
  * @author hoojo
  * @createDate 2019年1月2日 下午10:49:31
  * @file Keyboard.java
@@ -12,10 +12,10 @@ package io.github.hooj0.visitor.support;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public class Keyboard extends AbstractComputerComponent {
+public class Keyboard extends AbstractElectronicDevice {
 
 	@Override
-	public void accept(ComputerComponentVisitor componentVisitor) {
+	public void accept(ElectronicDeviceVisitor deviceVisitor) {
 		
 	}
 }
