3da58a1a1150048b62a010f28d42c0e4648c5982
hoojo <hoojo@qq.com>
2019-01-02 22:57:19 +0800

    :sparkles: :bento: :tada:  Init add keyboard visitor element

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Keyboard.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * keyboard visitor element
+ * 
+ * @author hoojo
+ * @createDate 2019年1月2日 下午10:49:31
+ * @file Keyboard.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Keyboard extends AbstractComputerComponent {
+
+	@Override
+	public void accept(ComputerComponentVisitor componentVisitor) {
+		
+	}
+}
