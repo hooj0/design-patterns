f6dca78b07a281c01d523617c4569936630234de
hoojo <hoojo@qq.com>
2019-01-02 22:57:21 +0800

    :sparkles: :bento: :tada:  Init add mouse visitor element

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Mouse.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * mouse visitor element 
+ * @author hoojo
+ * @createDate 2019年1月2日 下午10:51:25
+ * @file Mouse.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Mouse extends AbstractComputerComponent {
+
+	@Override
+	public void accept(ComputerComponentVisitor componentVisitor) {
+		
+	}
+}
