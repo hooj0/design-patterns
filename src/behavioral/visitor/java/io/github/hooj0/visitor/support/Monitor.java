bd01618b671204ad2f220993548f4be5d61d01e0
hoojo <hoojo@qq.com>
2019-01-02 22:57:20 +0800

    :sparkles: :bento: :tada:  Init add monitor visitor element

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Monitor.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * monitor visitor element
+ * @author hoojo
+ * @createDate 2019年1月2日 下午10:55:49
+ * @file Monitor.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Monitor extends AbstractComputerComponent {
+
+	@Override
+	public void accept(ComputerComponentVisitor componentVisitor) {
+		
+	}
+}
