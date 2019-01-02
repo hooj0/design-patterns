7a253d0ed6e72ebb2aff6bb2aea12bca2fae44a6
hoojo <hoojo@qq.com>
2019-01-02 21:52:21 +0800

    :sparkles: :bento: :truck:  Renamed visitor pattern electronic device

--- src/behavioral/visitor/java/io/github/hooj0/visitor/support/ElectronicDevice.java
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/ElectronicDevice.java
@@ -1,19 +1,19 @@
 package io.github.hooj0.visitor.support;
 
 /**
- * visitor pattern component 
+ * visitor pattern electronic device 
  * Element 角色接口对象
  * 
  * @author hoojo
  * @createDate 2019年1月2日 下午10:35:22
- * @file ComputerComponent.java
+ * @file ElectronicDevice.java
  * @package io.github.hooj0.visitor.support
  * @project design-patterns
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
  * @version 1.0
  */
-public interface ComputerComponent {
+public interface ElectronicDevice {
 
-	public void accept(ComputerComponentVisitor componentVisitor);
+	public void accept(ElectronicDeviceVisitor deviceVisitor);
 }
