7e4c4fd961bbc575a8921e062d0b523d2074ece0
hoojo <hoojo@qq.com>
2019-01-02 21:52:19 +0800

    :sparkles: :bento: :truck:  Renamed visitor pattern element abstract support

--- src/behavioral/visitor/java/io/github/hooj0/visitor/support/AbstractElectronicDevice.java
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/AbstractElectronicDevice.java
@@ -5,13 +5,13 @@ package io.github.hooj0.visitor.support;
  * Element 角色抽象实现
  * @author hoojo
  * @createDate 2019年1月2日 下午10:35:58
- * @file AbstractComputerComponent.java
+ * @file AbstractElectronicDevice.java
  * @package io.github.hooj0.visitor.support
  * @project design-patterns
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
  * @version 1.0
  */
-public abstract class AbstractComputerComponent implements ComputerComponent {
+public abstract class AbstractElectronicDevice implements ElectronicDevice {
 
 }
