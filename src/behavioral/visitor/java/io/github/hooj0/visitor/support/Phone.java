97de98cbc715e2964211a4511887c85f020f0268
hoojo <hoojo@qq.com>
2019-01-03 22:02:23 +0800

    :sparkles: :bento: :tada:  Init add phone component visitor element

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/Phone.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * phone component visitor element
+ * 手机  Element 抽象实现
+ * @author hoojo
+ * @createDate 2019年1月3日 下午9:58:02
+ * @file Phone.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class Phone extends AbstractElectronicDevice {
+
+	@Override
+	public void accept(ElectronicDeviceVisitor deviceVisitor) {
+		
+	}
+}
