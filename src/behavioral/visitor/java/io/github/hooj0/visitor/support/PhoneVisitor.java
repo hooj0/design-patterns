1da04bb254c07f24282568073e4d40ef1f76be80
hoojo <hoojo@qq.com>
2019-01-03 22:02:24 +0800

    :sparkles: :bento: :tada:  Init add phone visitor Electronic Device implements

---
+++ src/behavioral/visitor/java/io/github/hooj0/visitor/support/PhoneVisitor.java
@@ -0,0 +1,26 @@
+package io.github.hooj0.visitor.support;
+
+/**
+ * phone visitor Electronic Device implements
+ * phone 电子设备 具体的访问类
+ * @author hoojo
+ * @createDate 2019年1月3日 下午10:00:42
+ * @file PhoneVisitor.java
+ * @package io.github.hooj0.visitor.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class PhoneVisitor implements ElectronicDeviceVisitor {
+
+	@Override
+	public void visitComputer(Computer computer) {
+		
+	}
+
+	@Override
+	public void visitPhone(Phone phone) {
+		
+	}
+}
