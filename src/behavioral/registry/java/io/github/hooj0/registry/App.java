64171169ce336956e59f76f174405fd27c0ca809
hoojo <hoojo@qq.com>
2019-07-31 16:25:02 +0800

    :sparkles: :bento: :tada:  Init add registry design pattern application client

---
+++ src/behavioral/registry/java/io/github/hooj0/registry/App.java
@@ -0,0 +1,19 @@
+package io.github.hooj0.registry;
+
+/**
+ * registry design pattern application client
+ * @author hoojo
+ * @createDate 2019年8月1日 下午4:23:48
+ * @file App.java
+ * @package io.github.hooj0.registry
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+}
