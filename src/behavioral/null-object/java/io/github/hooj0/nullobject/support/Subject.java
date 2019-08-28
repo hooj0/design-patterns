3d29b66696a202dc0cc4260b50e815bbc8418fec
hoojo <hoojo@qq.com>
2019-08-28 11:13:16 +0800

    :sparkles: :bento: :tada:  Init add null-object pattern — subject interface class defined

---
+++ src/behavioral/null-object/java/io/github/hooj0/nullobject/support/Subject.java
@@ -0,0 +1,21 @@
+package io.github.hooj0.nullobject.support;
+
+/**
+ * null-object pattern — subject interface class defined
+ * 定义空对象主题接口
+ * 
+ * @author hoojo
+ * @createDate 2019年8月27日 上午10:55:37
+ * @file Subject.java
+ * @package io.github.hooj0.nullobject.support
+ * @project design-patterns
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface Subject {
+
+	public void start();
+	
+	public void stop();
+}
